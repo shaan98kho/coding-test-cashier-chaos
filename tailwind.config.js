297871bbb6797b47e9bc6d03feaf3ed1e6e6fc27
Wen Qi Kho <shaankho1998@gmail.com>
2025-12-16 15:21:58 +0800

    fix: added config for tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/gamez/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // triggers when viewport height is "tall"
+        tall: { raw: "(min-height: 800px)" },
+      },
+    },
   },
   plugins: [],
 };
